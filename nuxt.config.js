52359a993e5cef6c4a7d74b4ee1cca1a5b7a77a9
Kevin <kevinbillerach@yahoo.fr>
2023-02-27 18:38:18 +0100

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  // env: {
-  //   baseUrl: process.env.BASE_URL || 'http://localhost:1000'
-  // },
-
-  publicRuntimeConfig: {
-    baseURL: process.env.BASE_URL
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:1000'
   },
+
 
 
   // Global page headers: https://go.nuxtjs.dev/config-head
